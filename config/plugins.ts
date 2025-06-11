871bf66f81babdbbbe123bec1eb4a453cbd0a151
sherer <a.sherer@phlora.ru>
2025-06-11 17:37:24 +0300

    feat: Switch upload provider to AWS S3
    Reconfigured the upload plugin to use AWS S3 instead of local storage. This change enables cloud storage for uploaded files, utilizing environment variables for AWS credentials and bucket details.

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,20 +1,16 @@
 export default ({ env }) => ({
   upload: {
     config: {
-      provider: "local", // Используем локальное хранилище
-      // providerOptions для aws-s3 больше не нужны:
-      // providerOptions: {
-      //   rootPath: env("DO_SPACE_PATH"),
-      //   credentials: {
-      //     accessKeyId: env("DO_SPACE_ACCESS_KEY"),
-      //     secretAccessKey: env("DO_SPACE_SECRET_KEY"),
-      //   },
-      //   region: env("DO_SPACE_REGION"),
-      //   endpoint: env("DO_SPACE_ENDPOINT"),
-      //   params: {
-      //     Bucket: env("DO_SPACE_BUCKET"),
-      //   },
-      // },
+      provider: "aws-s3", // Изменяем провайдера на aws-s3
+      providerOptions: {
+        accessKeyId: env("AWS_ACCESS_KEY_ID"),
+        secretAccessKey: env("AWS_ACCESS_SECRET_KEY"),
+        region: env("AWS_REGION"),
+        endpoint: env("AWS_ENDPOINT"), // Обязательно для Timeweb Cloud
+        params: {
+          Bucket: env("AWS_BUCKET"),
+        },
+      },
       actionOptions: {
         upload: {},
         uploadStream: {},
