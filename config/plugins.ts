e9836f245cbd4e61f280c66697a5400908fded6b
maciej <maciej.golaszewski@rigbyjs.com>
2026-02-26 17:19:01 +0100

    Minio

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,17 +1,20 @@
 export default ({ env }) => ({
 	upload: {
 		config: {
-			provider: '@avorati/strapi-provider-upload-minio',
+			provider: 'aws-s3',
 			providerOptions: {
-				host: process.env.MINIO_HOST || process.env.MINIO_ENDPOINT || 'localhost',
-				port: process.env.MINIO_PORT ? parseInt(process.env.MINIO_PORT) : undefined,
-				// If port is not specified, the provider will automatically use the default port based on useSSL (443 for HTTPS, 9000 for HTTP)
-				useSSL: process.env.MINIO_USE_SSL === 'true',
-				rejectUnauthorized: process.env.MINIO_REJECT_UNAUTHORIZED !== 'false', // default: true (secure)
-				accessKey: process.env.MINIO_ACCESS_KEY,
-				secretKey: process.env.MINIO_SECRET_KEY,
-				bucket: process.env.MINIO_BUCKET || 'strapi-uploads',
-				folder: process.env.MINIO_FOLDER || '', // optional
+				s3Options: {
+					credentials: {
+						accessKeyId: env('MINIO_ACCESS_KEY_ID'),
+						secretAccessKey: env('MINIO_ACCESS_SECRET'),
+					},
+					region: env('MINIO_REGION'),
+					endpoint: env('MINIO_ENDPOINT'),
+					forcePathStyle: true, // Required for these providers
+					params: {
+						Bucket: env('MINIO_BUCKET'),
+					},
+				},
 			},
 		},
 	},
